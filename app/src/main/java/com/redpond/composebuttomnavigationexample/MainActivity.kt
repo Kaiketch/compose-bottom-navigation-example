f606b609d40bdb5f18a5e780299b54fb793e0c29
Kaiketch <akaike@kumofune.jp>
2021-11-29 16:50:12 +0900

    create project

---
+++ app/src/main/java/com/redpond/composebuttomnavigationexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.redpond.composebuttomnavigationexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
