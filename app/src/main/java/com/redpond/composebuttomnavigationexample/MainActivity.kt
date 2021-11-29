7821335764d68f7763ddffa170b9b07935656869
Kaiketch <akaike@kumofune.jp>
2021-11-29 17:22:14 +0900

    add compose

--- app/src/main/java/com/redpond/composebuttomnavigationexample/MainActivity.kt
+++ app/src/main/java/com/redpond/composebuttomnavigationexample/MainActivity.kt
@@ -2,10 +2,31 @@ package com.redpond.composebuttomnavigationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MaterialTheme {
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent(){
+    Column {
+        Text(text = "text", color = Color.White)
     }
 }
