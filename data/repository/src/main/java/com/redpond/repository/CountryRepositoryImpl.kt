1589412b929f139db98622c12a33534486ca2e2d
Kaiketch <akaike@kumofune.jp>
2022-01-11 19:00:05 +0900

    add country api

--- data/repository/src/main/java/com/redpond/repository/CountryRepositoryImpl.kt
+++ data/repository/src/main/java/com/redpond/repository/CountryRepositoryImpl.kt
@@ -1,9 +1,6 @@
 package com.redpond.repository
 
-import com.redpond.api.CountryApi
-import com.redpond.domain.User
 import com.redpond.domain.repository.CountryRepository
-import com.redpond.domain.repository.UserRepository
 import com.redpond.fragment.Country
 import javax.inject.Inject
 
