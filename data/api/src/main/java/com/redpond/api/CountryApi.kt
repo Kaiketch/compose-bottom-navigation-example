e0b5970d967ebc32d27f16c35b4bc1d9b53e5efd
Kaiketch <akaike@kumofune.jp>
2022-01-21 15:17:57 +0900

    modify country api

--- data/api/src/main/java/com/redpond/api/CountryApi.kt
+++ data/api/src/main/java/com/redpond/api/CountryApi.kt
@@ -2,6 +2,7 @@ package com.redpond.api
 
 import com.apollographql.apollo3.ApolloClient
 import com.redpond.CountriesQuery
+import com.redpond.CountryQuery
 import com.redpond.fragment.Country
 import javax.inject.Inject
 
@@ -12,5 +13,11 @@ class CountryApi @Inject constructor(
         val data = apolloClient.query(CountriesQuery()).execute().data
             ?: throw IllegalStateException("response data is null.")
         return data.countries.map { it.country }
+    }
+
+    suspend fun fetchCountry(code: String): Country {
+        val data = apolloClient.query(CountryQuery(code)).execute().data?.country
+            ?: throw IllegalStateException("response data is null.")
+        return data.country
     }
 }
