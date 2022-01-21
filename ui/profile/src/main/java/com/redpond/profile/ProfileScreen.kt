09ca0b8e7ef2c7024d6f5cb432d7616d44dc56d4
Kaiketch <akaike@kumofune.jp>
2022-01-21 15:14:13 +0900

    modify profile screen

--- ui/profile/src/main/java/com/redpond/profile/ProfileScreen.kt
+++ ui/profile/src/main/java/com/redpond/profile/ProfileScreen.kt
@@ -1,12 +1,28 @@
 package com.redpond.profile
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.redpond.common.component.ItemList
 
 @Composable
-fun ProfileScreen(
-    profileViewModel: ProfileViewModel = hiltViewModel()
-) {
-    ItemList(text = profileViewModel.get().name)
+fun ProfileScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.screen_profile),
+                    )
+                }
+            )
+        }
+    ) {
+        Text(
+            text = stringResource(id = R.string.screen_profile),
+        )
+    }
 }
