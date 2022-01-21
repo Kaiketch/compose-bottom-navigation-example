392bffc22c6928d2a2dfddf2e595fd3e9ab43fb9
Kaiketch <akaike@kumofune.jp>
2022-01-21 15:32:05 +0900

    move screen class

---
+++ ui/common/src/main/java/com/redpond/common/Screen.kt
@@ -0,0 +1,27 @@
+package com.redpond.common
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(
+    val route: String,
+    @StringRes val resourceId: Int,
+    val icon: ImageVector? = null
+) {
+    object Search : Screen("screen_search", R.string.screen_search, Icons.Filled.Search)
+    object Favorite : Screen("screen_favorite", R.string.screen_favorite, Icons.Filled.Favorite)
+    object Profile : Screen("screen_account", R.string.screen_profile, Icons.Filled.Person)
+    object Detail : Screen("screen_detail", R.string.screen_detail)
+}
+
+val bottomNavItems = listOf(
+    Screen.Search,
+    Screen.Favorite,
+    Screen.Profile,
+)
+
+const val CODE = "code"
