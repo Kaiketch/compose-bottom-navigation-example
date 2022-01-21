1a95e758a9acf387b28086abe94ac1a49968c227
Kaiketch <akaike@kumofune.jp>
2022-01-21 15:18:21 +0900

    remove unnecessary screens

--- ui/main/src/main/java/com/redpond/main/ScreenB.kt
+++
@@ -1,9 +0,0 @@
-package com.redpond.main
-
-import androidx.compose.runtime.Composable
-import com.redpond.common.component.ItemList
-
-@Composable
-fun ScreenB() {
-    ItemList(text = "B")
-}
--- ui/main/src/main/java/com/redpond/main/ScreenC.kt
+++
@@ -1,9 +0,0 @@
-package com.redpond.main
-
-import androidx.compose.runtime.Composable
-import com.redpond.common.component.ItemList
-
-@Composable
-fun ScreenC() {
-    ItemList(text = "C")
-}
