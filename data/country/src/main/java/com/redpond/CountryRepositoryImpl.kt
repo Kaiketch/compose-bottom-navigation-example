0134e7edb9a3346b52a96b4dca2241b8ee2a097c
Kaiketch <akaike@kumofune.jp>
2022-05-05 12:34:33 +0900

    modify country repository module name

---
+++ data/country/src/main/java/com/redpond/CountryRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.redpond
+
+import com.redpond.api.CountryApi
+import com.redpond.domain.repository.CountryRepository
+import com.redpond.fragment.Country
+import javax.inject.Inject
+
+internal class CountryRepositoryImpl @Inject constructor(
+    private val countryApi: CountryApi
+) : CountryRepository {
+    override suspend fun fetchCountries(): List<Country> {
+        return countryApi.fetchCountries()
+    }
+
+    override suspend fun fetchCountry(code: String): Country {
+        return countryApi.fetchCountry(code)
+    }
+}
