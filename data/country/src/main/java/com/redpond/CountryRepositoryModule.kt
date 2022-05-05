0c48a7f88855534f19f70c8dcecc799c58081a89
Kaiketch <akaike@kumofune.jp>
2022-05-05 12:35:15 +0900

    modify CountryRepositoryModule

---
+++ data/country/src/main/java/com/redpond/CountryRepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.redpond
+
+import com.redpond.domain.repository.CountryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CountryRepositoryModule {
+
+    @Singleton
+    @Binds
+    internal abstract fun provideCountryRepository(repository: CountryRepositoryImpl): CountryRepository
+}
