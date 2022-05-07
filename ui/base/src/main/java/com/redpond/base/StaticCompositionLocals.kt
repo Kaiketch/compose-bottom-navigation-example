8b388aecd5bf5bf3a033eddb934b309e6174d49d
Kaiketch <akaike@kumofune.jp>
2022-05-07 12:05:13 +0900

    add activity composition local

--- ui/base/src/main/java/com/redpond/base/StaticCompositionLocals.kt
+++ ui/base/src/main/java/com/redpond/base/StaticCompositionLocals.kt
@@ -1,8 +1,13 @@
 package com.redpond.base
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.navigation.NavController
 
 val LocalNavController = staticCompositionLocalOf<NavController> {
     error("NavController not found")
+}
+
+val LocalActivity = staticCompositionLocalOf<AppCompatActivity> {
+    error("Activity not found")
 }
