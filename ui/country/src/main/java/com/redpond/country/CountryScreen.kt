c2664feef7e1e9129377c837815999fbfb7cb919
Kaiketch <akaike@kumofune.jp>
2022-05-07 19:28:27 +0900

    modify CountryScreen

--- ui/country/src/main/java/com/redpond/country/CountryScreen.kt
+++ ui/country/src/main/java/com/redpond/country/CountryScreen.kt
@@ -1,6 +1,9 @@
 package com.redpond.country
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -8,13 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.redpond.base.LocalActivity
 import com.redpond.base.LocalNavController
+import com.redpond.base.viewmodel.UserViewModel
 
 @Composable
 fun CountryScreen(
+    userViewModel: UserViewModel = viewModel(LocalActivity.current),
     countryViewModel: CountryViewModel = hiltViewModel()
 ) {
     val navController = LocalNavController.current
@@ -44,23 +50,33 @@ fun CountryScreen(
                 .padding(it)
                 .padding(16.dp)
         ) {
+            Text(text = "code")
+            Text(text = uiState.country?.code.orEmpty())
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Text(text = "name")
             Text(text = uiState.country?.name.orEmpty())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = "capital",)
+            Text(text = "capital")
             Text(text = uiState.country?.capital.orEmpty())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = "currency",)
+            Text(text = "currency")
             Text(text = uiState.country?.currency.orEmpty())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = { }) {
-                Text(text = "Submit")
+            Button(
+                onClick = {
+                    userViewModel.onUpdateCountryClicked(uiState.country?.code)
+                    navController.popBackStack()
+                }
+            ) {
+                Text(text = "Update")
             }
         }
     }
