8209a0d48d67eaf2943102c8d13f4462b5d4fa0e
Kaiketch <akaike@kumofune.jp>
2022-05-07 17:57:58 +0900

    clean MainActivity

--- ui/main/src/main/java/com/redpond/main/MainActivity.kt
+++ ui/main/src/main/java/com/redpond/main/MainActivity.kt
@@ -2,25 +2,19 @@ package com.redpond.main
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
 import com.redpond.base.LocalActivity
-import com.redpond.base.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val userViewModel: UserViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        userViewModel.set("test")
 
         setContent {
             MaterialTheme {
